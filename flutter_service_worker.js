794ecc55248b5c5b85dd347c9a54985bf8e27726
devnejim <103866884+Nejim-jeb@users.noreply.github.com>
2022-05-09 01:52:43 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,8 +26,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "e91d60eb238b5016e49f0ae1d9ae9145",
-"/": "e91d60eb238b5016e49f0ae1d9ae9145",
+"index.html": "a77371246f3f4a7ebaeeed564cbf9565",
+"/": "a77371246f3f4a7ebaeeed564cbf9565",
 "main.dart.js": "00fb81ce87c75c6577d6ba2fc45c32ce",
 "manifest.json": "1764a02bda17457e2696f43973d5b9e6",
 "version.json": "ca1714706f8a1a1cf81fdfa61a969a07"
